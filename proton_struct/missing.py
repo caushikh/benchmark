e3fccb461268880a800c3d185d8d9e49379db2f7
Hari Caushik <hcaushik@gmail.com>
2013-10-24 23:33:18 -0700

    write missing structures to file

---
+++ proton_struct/missing.py
@@ -0,0 +1,18 @@
+import os
+
+files = os.listdir(os.getcwd())
+f = open('complexes.txt')
+miss = open('missing.txt','w')
+for line in f:
+	found = 0
+	line = line[0:4]
+	for i in files:
+		if (i[0:4] == line):
+			found = 1
+			break	
+	if not found:
+		print line, 'is missing'
+		miss.write(line + '\n')
+miss.close()
+f.close()
+	
